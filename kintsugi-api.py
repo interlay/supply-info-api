854e4f4c5b089d20d5fb546be9827edd32a2b9f0
alexei <alexei@interlay.io>
2022-03-16 13:14:38 +0100

    feat: added API key handling from file system with custom http header. Added kint total supply getter

--- kintsugi-api.py
+++ kintsugi-api.py
@@ -3,27 +3,50 @@ import requests
 
 SUBSCAN_URL = "https://kintsugi.api.subscan.io/"
 
+API_KEY_PATH = "api-key.txt"
+
+with open(API_KEY_PATH, 'r') as file:
+    API_KEY = file.read().rstrip()
+
+
 def from_10_decimals(val):
     return val / 1_000_000_000_000
 
 def from_8_decimals(val):
     return val / 10_000_000_000
 
+def subscan_get_request(url):
+    headers_dict = {
+        'content-type': 'application/json',
+        'X-API-KEY' : API_KEY}
+    return requests.get(url)
+    
+
+def subscan_post_request(url, json):
+    headers_dict = {
+        'content-type': 'application/json',
+        'X-API-KEY' : API_KEY}
+    return requests.post(url,json=json)
+    
+
 api = Flask(__name__)
 
-@api.route('/kint-supply', methods=['GET'])
-def get_kint_supply():
-    token_info_subscan = requests.get(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
+@api.route('/kint-circ-supply', methods=['GET'])
+def get_kint_circ_supply():
+    token_info_subscan = subscan_get_request(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
     unvested_supply = from_10_decimals(int(token_info_subscan["KINT"]["available_balance"]))
-    system_accounts_subscan = requests.post("https://kintsugi.api.subscan.io/api/scan/accounts", json={"filter": "system", "row": 25,"page": 0}).json()["data"]["list"]
+    system_accounts_subscan = subscan_post_request(SUBSCAN_URL + "api/scan/accounts", json={"filter": "system", "row": 25,"page": 0}).json()["data"]["list"]
     system_accounts_supply = sum([float(a["balance"]) for a in system_accounts_subscan])
     circulating_supply = unvested_supply - system_accounts_supply
     return str(circulating_supply)
 
+@api.route('/kint-total-supply', methods=['GET'])
+def get_kint_total_supply():
+    return str(10_000_000)
         
 @api.route('/kbtc-supply', methods=['GET'])
 def get_kbtc_supply():
-    token_info_subscan = requests.get(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
+    token_info_subscan = subscan_get_request(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
     kBTC_supply = from_10_decimals(int(token_info_subscan["KBTC"]["available_balance"]))
     return str(kBTC_supply)
 
