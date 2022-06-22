144cf132f2b5007e33a346bf146c1a3ce5756424
alexei <alexei@interlay.io>
2022-06-22 17:50:23 +0200

    feat: added intr circ supply

--- interlay-api.py
+++ interlay-api.py
@@ -2,12 +2,14 @@ from flask import Flask, json
 import requests
 import os
 
-SUBSCAN_URL = "https://kintsugi.api.subscan.io/"
+SUBSCAN_URL = "https://interlay.api.subscan.io/"
 
 API_KEY = os.environ.get('SUBSCAN_API_KEY')
 
-def from_12_decimals(val):
-    return val / 1_000_000_000_000
+CROWDLOAN_VESTING_END = 4838400
+
+def from_10_decimals(val):
+    return val / 10_000_000_000
 
 def from_8_decimals(val):
     return val / 100_000_000
@@ -26,28 +28,33 @@ def subscan_post_request(url, json):
     return requests.post(url,json=json)
     
 
+def max_crowdloan_vested():
+    current_block = int(subscan_get_request(SUBSCAN_URL + "api/scan/metadata").json()["data"]["blockNum"])
+    return 0.3 + (current_block / CROWDLOAN_VESTING_END)
+
 api = Flask(__name__)
 
-@api.route('/kint-circ-supply', methods=['GET'])
-def get_kint_circ_supply():
+@api.route('/intr-circ-supply', methods=['GET'])
+def get_intr_circ_supply():
     token_info_subscan = subscan_get_request(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
-    unvested_supply = from_12_decimals(float(token_info_subscan["KINT"]["available_balance"]))
-    print(unvested_supply)
+    unvested_supply = from_10_decimals(int(token_info_subscan["INTR"]["available_balance"]))
+    print(token_info_subscan["INTR"]["available_balance"])
     system_accounts_subscan = subscan_post_request(SUBSCAN_URL + "api/scan/accounts", json={"filter": "system", "row": 25,"page": 0}).json()["data"]["list"]
     system_accounts_supply = sum([float(a["balance"]) for a in system_accounts_subscan])
-    print(system_accounts_supply)
     circulating_supply = unvested_supply - system_accounts_supply
+    print(max_crowdloan_vested())
     return str(circulating_supply)
 
-@api.route('/kint-total-supply', methods=['GET'])
+@api.route('/intr-total-supply', methods=['GET'])
 def get_kint_total_supply():
-    return str(10_000_000)
+    return str(1_000_000_000)
         
-@api.route('/kbtc-supply', methods=['GET'])
+@api.route('/ibtc-supply', methods=['GET'])
 def get_kbtc_supply():
     token_info_subscan = subscan_get_request(SUBSCAN_URL + "api/scan/token").json()["data"]["detail"]
-    kBTC_supply = from_8_decimals(int(token_info_subscan["KBTC"]["available_balance"]))
+    kBTC_supply = from_10_decimals(int(token_info_subscan["IBTC"]["available_balance"]))
     return str(kBTC_supply)
 
 if __name__ == '__main__':
-    api.run() 
+    api.run(debug=True) 
+
